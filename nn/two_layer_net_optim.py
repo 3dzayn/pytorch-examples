e751dc81c40b525e93a8a2028134db42e532f122
Boris Dayma <boris.dayma@gmail.com>
2019-09-02 05:27:54 -0500

    feat: integrate with wandb

--- nn/two_layer_net_optim.py
+++ nn/two_layer_net_optim.py
@@ -1,4 +1,5 @@
 import torch
+import wandb
 
 """
 A fully-connected ReLU network with one hidden layer, trained to predict y from x
@@ -12,19 +13,23 @@ for us. The optim package defines many optimization algorithms that are commonly
 used for deep learning, including SGD+momentum, RMSProp, Adam, etc.
 """
 
+# Starts logging on W&B
+wandb.init(project="pytorch-examples")
+config = wandb.config  # shortcut for logging hyper-parameters
+
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
+config.N, config.D_in, config.H, config.D_out = 64, 1000, 100, 10
 
 # Create random Tensors to hold inputs and outputs.
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
+x = torch.randn(config.N, config.D_in)
+y = torch.randn(config.N, config.D_out)
 
 # Use the nn package to define our model and loss function.
 model = torch.nn.Sequential(
-          torch.nn.Linear(D_in, H),
+          torch.nn.Linear(config.D_in, config.H),
           torch.nn.ReLU(),
-          torch.nn.Linear(H, D_out),
+          torch.nn.Linear(config.H, config.D_out),
         )
 loss_fn = torch.nn.MSELoss(reduction='sum')
 
@@ -32,8 +37,8 @@ loss_fn = torch.nn.MSELoss(reduction='sum')
 # the model for us. Here we will use Adam; the optim package contains many other
 # optimization algoriths. The first argument to the Adam constructor tells the
 # optimizer which Tensors it should update.
-learning_rate = 1e-4
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+config.learning_rate = 1e-4
+optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
 for t in range(500):
   # Forward pass: compute predicted y by passing x to the model.
   y_pred = model(x)
@@ -41,6 +46,9 @@ for t in range(500):
   # Compute and print loss.
   loss = loss_fn(y_pred, y)
   print(t, loss.item())
+
+  # Log to W&B
+  wandb.log({'loss': loss.item()})
   
   # Before the backward pass, use the optimizer object to zero all of the
   # gradients for the Tensors it will update (which are the learnable weights
